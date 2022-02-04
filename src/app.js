6c26364209fddee7e094d00789560e5cb3c4ac0a
Hicka <cc@hicka.world>
2022-02-04 19:52:31 +0100

    fix(backend): fix paths

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 require('dotenv').config(); // Populates process.env with values set in .env
 const express = require('express');
+const path = require('path');
 const Logger = require('./lib/misc/Logger');
 const app = express();
 
-app.use('view engine', 'pug');
-app.use('views', './views');
-app.use('/public', express.static('../public'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 
 app.listen(process.env.PORT || 3000, () => {
 	new Logger('App').info('Server started');
