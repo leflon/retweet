e1c29055194b8ca4571e80d5561ad7e396f21072
hickatheworld <cc@hicka.world>
2022-04-27 22:14:07 +0200

    mdr ce fichier sert à rien

--- public/js/api.js
+++
@@ -1,9 +0,0 @@
-const like = async (id) => {
-	const t = await fetch(`/api/tweets/like/${id}`);
-	console.log(t);
-};
-
-const retweet = async (id) => {
-	const t = await fetch(`/api/tweets/retweet/${id}`);
-	console.log(t);
-};
