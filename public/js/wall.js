6945997f75c934c7d2984244bbb7e755b6c7f41e
hickatheworld <cc@hicka.world>
2022-05-10 11:20:31 +0200

    fix(wall): fix inputs feedback

--- public/js/wall.js
+++ public/js/wall.js
@@ -4,45 +4,51 @@ if (MODE === 'register') {
 	const form = document.getElementById('register-form');
 
 	const inputs = {
-		email: document.getElementById('email'),
-		username: document.getElementById('username'),
-		password: document.getElementById('password'),
-		confirm: document.getElementById('pw-confirm'),
+		email: document.getElementById('text-input-email'),
+		username: document.getElementById('text-input-username'),
+		password: document.getElementById('text-input-password'),
+		confirm: document.getElementById('text-input-pw-confirm'),
 	};
 	const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 	form.addEventListener('submit', e => {
 		let anyError = false;
 		// E-mail checks
-		if (!inputs.email.value.match(emailRegex)) {
-			inputs.email.classList.add('is-invalid');
+		const email = inputs.email.querySelector('input').value;
+		if (!email.match(emailRegex)) {
+			inputs.email.classList.add('invalid');
 			anyError = true;
-		} else if (inputs.email.classList.contains('is-invalid'))
-			inputs.email.classList.remove('is-invalid');
+		} else if (inputs.email.classList.contains('invalid'))
+			inputs.email.classList.remove('invalid');
 		// Username checks
-		if (inputs.username.value.length < 3 || inputs.username.value.length > 16 || !/^[a-z\d_]+$/i.test(inputs.username.value)) {
-			inputs.username.classList.add('is-invalid');
+		const username = inputs.username.querySelector('input').value;
+		if (username.length < 3
+			|| username.length > 16
+			|| !/^[a-z\d_]+$/i.test(username)) {
+			inputs.username.classList.add('invalid');
 			anyError = true;
-		} else if (inputs.username.classList.contains('is-invalid'))
-			inputs.username.classList.remove('is-invalid');
+		} else if (inputs.username.classList.contains('invalid'))
+			inputs.username.classList.remove('invalid');
 
 		// Password checks
-		if (inputs.password.value.length < 8 || inputs.password.value.length > 50 ||
-			/^[a-z A-Z\p{L}\p{M}\d]+$/gu.test(inputs.password.value) || // true if no symbol
-			!/\d/.test(inputs.password.value) || // true if no number
-			!/[A-Z]/.test(inputs.password.value) || // true if no capital letter
-			!/[a-z]/.test(inputs.password.value)) { // true if no lowercase letter
-			inputs.password.classList.add('is-invalid');
+		const password = inputs.password.querySelector('input').value;
+		if (password.length < 8 || password.length > 50 ||
+			/^[a-z A-Z\p{L}\p{M}\d]+$/gu.test(password) || // true if no symbol
+			!/\d/.test(password) || // true if no number
+			!/[A-Z]/.test(password) || // true if no capital letter
+			!/[a-z]/.test(password)) { // true if no lowercase letter
+			inputs.password.classList.add('invalid');
 			anyError = true;
-		} else if (inputs.password.classList.contains('is-invalid'))
-			inputs.password.classList.remove('is-invalid');
+		} else if (inputs.password.classList.contains('invalid'))
+			inputs.password.classList.remove('invalid');
 
 		// Confirm password checks
-		if (inputs.password.value !== inputs.confirm.value) {
-			inputs.confirm.classList.add('is-invalid');
+		const passwordConfirmed = inputs.confirm.querySelector('input').value;
+		if (passwordConfirmed !== password) {
+			inputs.confirm.classList.add('invalid');
 			anyError = true;
-		} else if (inputs.confirm.classList.contains('is-invalid'))
-			inputs.confirm.classList.remove('is-invalid');
-
+		} else if (inputs.confirm.classList.contains('invalid'))
+			inputs.confirm.classList.remove('invalid');
+		console.log({username, email, password, passwordConfirmed});
 		if (anyError)
 			e.preventDefault();
 	});
